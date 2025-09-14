aedd74f6f537a64bcb5e6573bd0a2f35887b221f
Nicolás Yorlano <nicolasyorlano.arg@gmal.com>
2025-09-14 18:56:27 -0300

    --

--- src/api/institution.api.js
+++ src/api/institution.api.js
@@ -1,17 +1,16 @@
-import { loremTmp } from "./data/lorem.tmp.js";
 
 const KEY_INSTITUTION = "institution";
 
 const initialize = () => {
     const initialData = {
-        name: "Mi App",
-        address: "Av. Siempreviva 100, San Juan, Argentina",
+        name: "Hamburguesas LP",
+        address: "La Plata 123, Buenos Aires, Argentina.",
         phone: "264-411-2233",
-        email: "info@miapp.com",
+        email: "HamburguesasLaPlata@gmial.com",
         about: {
-            mission: loremTmp,
-            vision: loremTmp,
-            values: loremTmp,
+            mission: "En Hamburguesas LP nos proponemos crear hamburguesas de calidad, usando ingredientes frescos y ofreciendo un servicio rápido y cercano para que cada cliente disfrute de una experiencia única.",
+            vision: "Queremos que Hamburguesas LP sea reconocida como la hamburguesería de referencia en la La Plata, destacándonos por nuestro sabor, la innovación y la forma en que conectamos con cada persona.",
+            values: "Nos guiamos por la calidad en cada producto, la innovación constante para mejorar, la cercanía con clientes y colaboradores, y la responsabilidad hacia la comunidad y el medio ambiente.",
         },
     };
 
--- src/components/layout/header/institution-logo/InstitutionLogo.jsx
+++ src/components/layout/header/institution-logo/InstitutionLogo.jsx
@@ -8,7 +8,6 @@ const InstitutionLogo = (props) => {
 
     return (
         <div className={classes} {...restProps}>
-            <img className="institution-logo__logo" src={"/images/logo.png"} alt="Logo de la institución"/>
             <Text className="institution-logo__title" variant="h1">Hamburguesas LP</Text>
         </div>
     );
