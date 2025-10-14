51dd659074bf2ad51336748c32b934a57d326988
Nicolás Yorlano <nicolasyorlano.arg@gmal.com>
2025-10-14 14:11:34 -0300

    -

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express from "express";
+import { config as configJson } from "./config/json.config.js";
+import { connectDB } from "./config/mongoose.config.js";
+import { config as configStatic } from "./config/static.config.js";
+
+import inquiryRouter from "./routers/inquiry.router.js";
+import productRouter from "./routers/product.router.js";
+import institutionRouter from "./routers/institution.router.js";
+
+const app = express();
+configJson(app);
+configStatic(app);
+connectDB();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+// Declaración de rutas
+app.use("/api/products", productRouter);
+app.use("/api/inquiry", inquiryRouter);
+app.use("/api/institutions", institutionRouter);
+
+// Control de rutas inexistentes
+app.use((req, res) => {
+    res.status(404).send("<h1>Error 404</h1><h3>La URL indicada no existe en este servidor</h3>");
+});
+
+// Método oyente de solicitudes
+app.listen(PORT, HOST, () => {
+    console.log(`Ejecutándose en http://${HOST}:${PORT}`);
+});
