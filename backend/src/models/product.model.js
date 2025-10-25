ce68985d21c42c4b899f9020aa2e2ff26e0097a7
Nicolás Yorlano <nicolasyorlano.arg@gmal.com>
2025-10-25 14:55:32 -0300

    -

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -36,8 +36,8 @@ const productSchema = new Schema({
         default: false,
     },
 }, {
-    timestamps: true, // Creará dos campos: createdAt y updatedAt
-    versionKey: false, // No creará el campo __v
+    timestamps: true, // Creará dos campos adicionales: createdAt y updatedAt
+    versionKey: false, // No creará el campo __v en cada registro
 });
 
 const ProductModel = model("products", productSchema);
