dde47227dd8c7950740c62a2ecbcad9071aad1f3
Nicolás Yorlano <nicolasyorlano.arg@gmal.com>
2025-10-28 18:22:58 -0300

    -

--- frontend/src/components/layout/header/institution-logo/InstitutionLogo.jsx
+++ frontend/src/components/layout/header/institution-logo/InstitutionLogo.jsx
@@ -1,14 +1,19 @@
 import { Text } from "@/components/texts";
+import AppContext from "@/contexts/AppContext";
 import PropTypes from "prop-types";
+import { useContext } from "react";
 import "./institution-logo.scss";
 
 const InstitutionLogo = (props) => {
     const { className, ...restProps } = props;
     const classes = `institution-logo ${className ?? ""}`;
 
+    const { institutionContext } = useContext(AppContext);
+    const { institution } = institutionContext;
+
     return (
         <div className={classes} {...restProps}>
-            {institution.name && (
+            {institution?.name && (
                 <>
                     <Text className="institution-logo__title" variant="h1">{institution.name}</Text>
                 </>
